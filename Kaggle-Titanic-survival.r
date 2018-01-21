6a3c0f13b2ea9b1065b2810e7bfb08934007ae77
Prabhat Kumar Jha <prabhuyesjha@gmail.com>
2018-01-20 20:40:31 -0600

    Add files via upload
    achieved accuracy ~80%

---
+++ Kaggle-Titanic-survival.r
@@ -0,0 +1,32 @@
+# This R environment comes with all of CRAN preinstalled, as well as many other helpful packages
+# The environment is defined by the kaggle/rstats docker image: https://github.com/kaggle/docker-rstats
+# For example, here's several helpful packages to load in 
+
+library(ggplot2) # Data visualization
+library(readr) # CSV file I/O, e.g. the read_csv function
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+system("ls ../input")
+
+# Any results you write to the current directory are saved as output.
+train=read.csv("../input/train.csv")
+str(train)
+table(train$Survived)
+prop.table(table(train$Survived))
+test=read.csv("../input/test.csv")
+test$Survived <- rep(0, 418)
+summary(train$Sex)
+prop.table(table(train$Sex, train$Survived),1)
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+test$Survived <- 0
+test$Survived[test$Sex == 'female'] <- 1
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "output.csv", row.names = FALSE)
